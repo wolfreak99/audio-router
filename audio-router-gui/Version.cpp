0bc44fc283daf3384363e524f4d624e66bfe2e37
wolfreak99 <jbminor1991@gmail.com>
2017-07-25 04:35:46 -0400

    Create Version files (to be used with appveyor)

---
+++ audio-router-gui/Version.cpp
@@ -0,0 +1,25 @@
+#include <string>
+#include "Version.h"
+
+// TODO/wolfreak99: Create "third party used:" or whatever people use to list code borrowed from other open source projects, and include link to OpenRCT2's license
+// Because right now I'm just trying to get this damned thing to work.
+
+#ifdef AUDIOROUTER_BUILD_INFO_HEADER
+#include AUDIOROUTER_BUILD_INFO_HEADER
+#endif
+
+const char gVersionInfoFull[] =
+AUDIOROUTER_NAME ", v" AUDIOROUTER_VERSION
+#ifdef AUDIOROUTER_BRANCH
+"-" AUDIOROUTER_BRANCH
+#endif
+#ifdef AUDIOROUTER_COMMIT_SHA1_SHORT
+" build " AUDIOROUTER_COMMIT_SHA1_SHORT
+#endif
+#ifdef AUDIOROUTER_BUILD_SERVER
+" provided by " AUDIOROUTER_BUILD_SERVER
+#endif
+#ifndef NDEBUG
+" (DEBUG)"
+#endif
+;
---
+++ audio-router-gui/Version.h
@@ -0,0 +1,39 @@
+#pragma once
+
+#include "..\audio-router\common.h"
+
+#define AUDIOROUTER_NAME               "AUDIOROUTER"
+#define AUDIOROUTER_VERSION            "0.1.1"
+
+#if defined(__amd64__) || defined(_M_AMD64)
+#define AUDIOROUTER_ARCHITECTURE       "x86-64"
+#elif defined(__i386__) || defined(_M_IX86)
+#define AUDIOROUTER_ARCHITECTURE       "x86"
+#elif defined(PLATFORM_64BIT)
+#define AUDIOROUTER_ARCHITECTURE       "64-bit"
+#elif defined(PLATFORM_32BIT)
+#define AUDIOROUTER_ARCHITECTURE       "32-bit"
+#endif
+
+#ifndef AUDIOROUTER_ARCHITECTURE
+#error "AUDIOROUTER_ARCHITECTURE is undefined. Please add identification."
+#endif
+
+// Platform
+#ifdef _WIN32
+#define AUDIOROUTER_PLATFORM       "Windows"
+#endif
+#ifndef AUDIOROUTER_PLATFORM
+#error Unknown platform!
+#endif
+
+#define AUDIOROUTER_TIMESTAMP          __DATE__ " " __TIME__
+
+#ifdef __cplusplus
+extern "C"
+{
+#endif
+    extern const char gVersionInfoFull[];
+#ifdef __cplusplus
+}
+#endif
